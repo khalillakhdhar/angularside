076ede0f025525b455e4c6e8a91af8c2f247692f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-02-25 20:16:29 +0100

    projet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { GestionComponent } from './gestion/gestion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path:"",component:ConnexionComponent},
+{path:"gestion",component:GestionComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/classes/customer.ts
@@ -0,0 +1,9 @@
+export class Customer {
+    id:number;
+    firstname:string;
+    lastname:string;
+    adress:string;
+    age:number;
+    createdAt:Date;
+    updatedAt:Date;
+}
