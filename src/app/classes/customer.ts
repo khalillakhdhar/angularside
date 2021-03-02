19b02119e385f3e48c5d97a23054bda21320f1f5
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-02 19:59:00 +0100

    Update customer.ts

--- src/app/classes/customer.ts
+++ src/app/classes/customer.ts
@@ -2,7 +2,7 @@ export class Customer {
     id:number;
     firstname:string;
     lastname:string;
-    adress:string;
+    address:string;
     age:number;
     createdAt:Date;
     updatedAt:Date;
