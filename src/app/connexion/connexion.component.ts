3bd7019ba23102e0ad4d954dfb1227f390735623
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-02 18:42:34 +0100

    code

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -10,24 +10,41 @@ import { CustomerService } from '../services/customer.service';
 })
 export class ConnexionComponent implements OnInit {
   costumers = new Array<Customer>();
-
+costumer:Customer;
   constructor(private customerService: CustomerService) { }
 
   ngOnInit() {
-
+this.costumer=new Customer();
     this.getusers();
-   // console.log(this.customers);
+    console.log(this.costumers);
   }
 
   getusers(): void {
     this.customerService.getUsers().subscribe(res => {
       this.costumers = res;
+      console.log(this.costumers);
+
     });
    }
    
   connect()
   {
+    this.customerService.getUsers().subscribe(res => {
+      this.costumers = res;
+      for(let cs of this.costumers)
+      {
+        if((cs.firstname===this.costumer.firstname)&&(cs.lastname===this.costumer.lastname))
+        {
+          window.location.replace("gestion");
+        }
+      }
+      console.log(this.costumers);
+
+    });
+
+
 // connexion via username et lastname
+
   }
 }
 
